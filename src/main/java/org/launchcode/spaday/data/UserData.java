890652a597cdf8456c50ea9023bb23f4e1142088
Matt Plog <ps3hawk@gmail.com>
2021-06-03 18:42:05 -0500

    Chapter-13-studio-done

---
+++ src/main/java/org/launchcode/spaday/data/UserData.java
@@ -0,0 +1,27 @@
+//package org.launchcode.spaday.data;
+//
+//import org.launchcode.spaday.models.User;
+//
+//import java.util.Collection;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class UserData {
+//    private static final Map<Integer, User> events = new HashMap<>();
+//
+//    public static Collection<User> getAll() {
+//        return events.values();
+//    }
+//
+//    public static  getUsername(String UserName) {
+//        return user.get;
+//    }
+//
+//    public static void add(User event) {
+//        events.put(event.getId(), event);
+//    }
+//
+//    public static void remove(int id) {
+//        events.remove(id);
+//    }
+//}
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,44 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Cannot be blank")
+    @Size(min = 5, max = 15, message = "Must be between 5 and 15 characters long")
     private String username;
+
+    @Email
     private String email;
+
+    @NotNull(message = "Passwords do not match")
+    public String verifyPassword;
+
+
+
+    @NotBlank(message = "Cannot be blank")
+    @Size(min = 6, message = "Must be 6 characters")
     private String password;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    private void checkPassword() {
+        if(password != null && verifyPassword != null && !password.equals(verifyPassword)){
+            verifyPassword = null;
+        }
     }
 
     public String getUsername() {
@@ -38,5 +63,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
